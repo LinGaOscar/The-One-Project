439ec3113538153dcb60213d8a6321659bd4932e
LinGaOscar <oscar987123@yahoo.com.tw>
2022-03-17 18:14:45 +0800

    set

--- OneAP/src/main/java/com/oscar/oneap/controller/Controller.java
+++ OneAP/src/main/java/com/oscar/oneap/controller/Controller.java
@@ -3,25 +3,28 @@ package com.oscar.oneap.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
- public class Controller {
+public class Controller {
 
-    @GetMapping("/login")
-    public Map<String, String> login() {
-        Map<String, String> result = Map.of("message", "login say hello");
+    @GetMapping("/")
+    public Map<String, String> SayHello() {
+        Map<String, String> result = Map.of("message", "say hello");
         return result;
     }
-     @GetMapping("/admin/hello")
-     public Map<String, String> adminSayHello() {
-         Map<String, String> result = Map.of("message", "admin say hello");
-         return result;
-     }
- 
-     @GetMapping("/user/hello")
-     public Map<String, String> userSayHello() {
-         Map<String, String> result = Map.of("message", "user say hello");
-         return result;
-     }
- }
+
+    @GetMapping("/user/hello")
+    public Map<String, String> userSayHello() {
+        Map<String, String> result = Map.of("message", "user say hello");
+        return result;
+    }
+
+    @GetMapping("/admin/hello")
+    public Map<String, String> adminSayHello() {
+        Map<String, String> result = Map.of("message", "admin say hello");
+        return result;
+    }
+
+}
